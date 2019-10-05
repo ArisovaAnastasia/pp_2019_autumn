52ccc59c0c906eb2aa30ab593120b9cfbfc5f5b2
ArisovaAnastasia <43183124+ArisovaAnastasia@users.noreply.github.com>
2019-10-05 13:56:33 +0300

    MPI_version_2.0

--- modules/task_1/arisova_a_rectangle_integration/main.cpp
+++ modules/task_1/arisova_a_rectangle_integration/main.cpp
@@ -1,64 +1,143 @@
-
+// Copyright 2019 Arisova Anasrasiia
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <vector>
 #include "./rectangle_integration.h"
-/*
-TEST(Parallel_Operations_MPI, Test_Sum) {
+
+double F1(double x) {
+    return x*x;
+}
+
+double F2(double x) {
+    return 2.0*x;
+}
+
+double F3(double x) {
+    return 1.0/(x * x);
+}
+
+TEST(SequentialIntegration, Test_F1) {
+    int N = 10000;
+    std::vector<double> vec(N + 1);
+    double a = 0.0, b = 2.0;
+    double h = (b - a)/(static_cast<double> (N));
+    for (int i = 0; i < N; i++) {
+            vec[i] = a + i * h;
+    }
+    vec[N] = b;
+    double sum = 0.0;
+    sum = getSequentialIntegration(vec, F1, h);
+
+      ASSERT_NEAR(2.667, sum, 1e-2);
+}
+
+TEST(SequentialIntegration, Test_F2) {
+    int N = 10000;
+    std::vector<double> vec(N + 1);
+    double a = 0.0, b = 2.0;
+    double h = (b - a)/(static_cast<double> (N));
+    for (int i = 0; i < N; i++) {
+            vec[i] = a + i * h;
+    }
+    vec[N] = b;
+    double sum = 0.0;
+    sum = getSequentialIntegration(vec, F2, h);
+
+      ASSERT_NEAR(4.0, sum, 1e-2);
+}
+
+TEST(Parallel_Integration_MPI, Test_MPI_F1) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> global_vec;
-    const int count_size_vector = 100;
+
+    int N = 10000;
+    std::vector<double> global_vector(N + 1);
+    double a = 0.0, b = 3.0;
+    double h = (b - a)/(static_cast<double> (N));
 
     if (rank == 0) {
-        global_vec = getRandomVector(count_size_vector);
+        for (int i = 0; i < N; i++) {
+            global_vector[i] = a + i * h;
+        }
+        global_vector[N] = b;
     }
-
-    int global_sum = getParallelOperations(global_vec, count_size_vector, "+");
+    double sum = 9.0;
+    double g_sum = getParallelIntegration(global_vector, F1, a, b, N);
 
     if (rank == 0) {
-        int reference_sum = getSequentialOperations(global_vec, "+");
-        ASSERT_EQ(reference_sum, global_sum);
+        ASSERT_NEAR(sum, g_sum, 1e-2);
     }
 }
 
-TEST(Parallel_Operations_MPI, Test_Diff) {
+TEST(Parallel_Integration_MPI, Test_MPI_F1_with_negative_values) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> global_vec;
-    const int count_size_vector = 100;
+
+    int N = 10000;
+    std::vector<double> global_vector(N + 1);
+    double a = -2.0, b = -1.0;
+    double h = (b - a)/(static_cast<double> (N));
 
     if (rank == 0) {
-        global_vec = getRandomVector(count_size_vector);
+        for (int i = 0; i < N; i++) {
+            global_vector[i] = a + i * h;
+        }
+        global_vector[N] = b;
     }
-
-    int global_diff = getParallelOperations(global_vec, count_size_vector, "-");
+    double sum = 7.0/3.0;
+    double g_sum = getParallelIntegration(global_vector, F1, a, b, N);
 
     if (rank == 0) {
-        int reference_diff = getSequentialOperations(global_vec, "-");
-        ASSERT_EQ(reference_diff, global_diff);
+        ASSERT_NEAR(sum, g_sum, 1e-2);
     }
 }
 
-TEST(Parallel_Operations_MPI, Test_Max) {
+TEST(Parallel_Integration_MPI, Test_MPI_F2) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> global_vec;
-    const int count_size_vector = 100;
+
+    int N = 10000;
+    std::vector<double> global_vector(N + 1);
+    double a = 0.0, b = 2.0;
+    double h = (b - a)/(static_cast<double> (N));
 
     if (rank == 0) {
-        global_vec = getRandomVector(count_size_vector);
+        for (int i = 0; i < N; i++) {
+            global_vector[i] = a + i * h;
+        }
+        global_vector[N] = b;
     }
-
-    int global_max;
-    global_max = getParallelOperations(global_vec, count_size_vector, "max");
+    double sum = 4.0;
+    double g_sum = getParallelIntegration(global_vector, F2, a, b, N);
 
     if (rank == 0) {
-        int reference_max = getSequentialOperations(global_vec, "max");
-        ASSERT_EQ(reference_max, global_max);
+        ASSERT_NEAR(sum, g_sum, 1e-2);
     }
 }
-*/
+
+TEST(Parallel_Integration_MPI, Test_MPI_F3) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int N = 10000;
+    std::vector<double> global_vector(N + 1);
+    double a = 1.0, b = 3.0;
+    double h = (b - a)/(static_cast<double> (N));
+
+    if (rank == 0) {
+        for (int i = 0; i < N; i++) {
+            global_vector[i] = a + i * h;
+        }
+        global_vector[N] = b;
+    }
+    double sum = 2.0/3.0;
+    double g_sum = getParallelIntegration(global_vector, F3, a, b, N);
+
+    if (rank == 0) {
+        ASSERT_NEAR(sum, g_sum, 1e-2);
+    }
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
--- modules/task_1/arisova_a_rectangle_integration/rectangle_integration.h
+++ modules/task_1/arisova_a_rectangle_integration/rectangle_integration.h
@@ -1,10 +1,10 @@
-
-#ifndef MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
-#define MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+// Copyright 2019 Arisova Anasrasiia
+#ifndef MODULES_TASK_1_ARISOVA_A_RECTANGLE_INTEGRATION_RECTANGLE_INTEGRATION_H_
+#define MODULES_TASK_1_ARISOVA_A_RECTANGLE_INTEGRATION_RECTANGLE_INTEGRATION_H_
 
 #include <vector>
 
-double getParallelIntegration(std::vector<double> vec, double* F, double a, double b, int N);
-double getSequentialIntegration(std::vector<double> vec, double* F, double l_h);
+double getParallelIntegration(std::vector<double> vec, double(*F)(double), double a, double b, int N);
+double getSequentialIntegration(std::vector<double> vec, double(*F)(double), double l_h);
 
-#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#endif  // MODULES_TASK_1_ARISOVA_A_RECTANGLE_INTEGRATION_RECTANGLE_INTEGRATION_H_
